0054414227362c359f402f51c57a5fb46babab23
kkai0828 <khsauian@gmail.com>
2026-01-08 14:45:48 +0800

    fix: Mobile phone time error cause wrong time zone

--- lib/ui/screens/my_bookings_screen.dart
+++ lib/ui/screens/my_bookings_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 // 🔥 請確認您的路徑正確
 import '../../data/models/booking_model.dart';
